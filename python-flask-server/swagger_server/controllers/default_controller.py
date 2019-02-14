cd94fb3e853aba1b57647c30edecad545fac0f6d
eli <elitsa.vr@gmail.com>
2019-02-14 14:15:40 +0100

    some changes

--- python-flask-server/swagger_server/controllers/default_controller.py
+++ python-flask-server/swagger_server/controllers/default_controller.py
@@ -5,7 +5,7 @@ from swagger_server.models.errorunknown import ERRORUNKNOWN  # noqa: E501
 from swagger_server.models.error import Error  # noqa: E501
 from swagger_server.models.persons import Persons  # noqa: E501
 from swagger_server import util
-
+from flask import request, abourt, Response, jsonify
 
 def persons_get(User_Agent, pageSize=None, pageNumber=None):  # noqa: E501
     """Gets some persons
@@ -23,25 +23,25 @@ def persons_get(User_Agent, pageSize=None, pageNumber=None):  # noqa: E501
     """
     return 'do some magic!'
 
+persons_list = []
 
-def persons_post(username, firstname=None, lastname=None, dateOfBirth=None):  # noqa: E501
+def persons_post(person=None):  # noqa: E501
     """Creates a person
 
     Adds a new person to the persons list. # noqa: E501
 
-    :param username: 
-    :type username: str
-    :param firstname: 
-    :type firstname: str
-    :param lastname: 
-    :type lastname: str
-    :param dateOfBirth: 
-    :type dateOfBirth: str
+    :param person: The person to create.
+    :type person: dict | bytes
 
     :rtype: None
     """
-    dateOfBirth = util.deserialize_date(dateOfBirth)
-    return 'do some magic!'
+    # dateOfBirth = util.deserialize_date(dateOfBirth)
+
+    if connexion.request.is_json:
+    person = Person.from_dict(connexion.request.get_json()) #noqa
+
+    person_list.append(person)
+    return Response(mimetype='application/json',status=204)
 
 
 def persons_username_delete():  # noqa: E501
